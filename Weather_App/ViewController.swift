7d34b73c67b3cbf278b86a5e20d62e4420ac408b
Durodola, Habib <Habib@compassdigital.io>
2017-09-10 17:06:01 -0400

    added table view

--- Weather_App/ViewController.swift
+++ Weather_App/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var names: [String] = []
+    var contacts: [String] = []
+    
     @IBAction func load(_ sender: UIButton) {
-        getLocationApi();
+        getWeatherApi();
     }
-    let urlString = URL(string: "http://jsonplaceholder.typicode.com/users/1")
+  // let urlString = URL(string: "http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=6e65fa567cc8107e28d13c931dce5db7")
+     let urlString=URL(string:"http://api.androidhive.info/contacts/")
     
     override func viewDidLoad() {
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,9 +32,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func getLocationApi(){
+    func getWeatherApi(){
         _ = [String]()
-        var namesArray = [String]()
+        _ = [String]()
         if let url = urlString {
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                 if error !=  nil {
@@ -40,31 +44,42 @@ class ViewController: UIViewController {
                     
                     do {
                         if data != nil {
-                            let json = try JSONSerialization.jsonObject(with: data!) as! [String: AnyObject]
-                            _ = json["name"] as? String
-                            let username = json["username"] as? String
-                            let email = json["email"] as? String
-                            let address = json["address"] as? [String : AnyObject]
-                            let street = address?["street"] as? String
-            
-                            let geo = address?["geo"] as? [String : AnyObject]
-                            let lat = geo?["lat"] as? String
-                            
-                            if let names = json["name"] as? String
-                            {
-                                namesArray.append(names)
-                                namesArray.append(username!)
-                                namesArray.append(email!)
-                                namesArray.append(street!)
-                                namesArray.append(lat!)
-                                
-                                
-                                print(names)
-                                print("olawepo in ")
+                          //  let json = try JSONSerialization.jsonObject(with: data!) as! [String: AnyObject]
+                            let allContactsData = try Data(contentsOf: url)
+                            let allContacts = try JSONSerialization.jsonObject(with: allContactsData, options: JSONSerialization.ReadingOptions.allowFragments) as! [String : AnyObject]
+                            if let arrJSON = allContacts["contacts"] {
+                                for index in 0...arrJSON.count-1 {
+                                    let aObject = arrJSON[index] as! [String : AnyObject]
+                                    self.names.append(aObject["name"] as! String)
+                                    self.contacts.append(aObject["email"] as! String)
+                                }
                             }
+                            print(self.names)
+                            print(self.contacts)
+                            
+                            
+                            //                            let username = json["username"] as? String
+                            //                            let email = json["email"] as? String
+                            //                            let address = json["address"] as? [String : AnyObject]
+                            //                            let street = address?["street"] as? String
+                            //                            let geo = address?["geo"] as? [String : AnyObject]
+                            //                            let lat = geo?["lat"] as? String
+                            //
+                            //                            if let names = json["name"] as? String
+                            //                            {
+                            //                                namesArray.append(names)
+                            //                                namesArray.append(username!)
+                            //                                namesArray.append(email!)
+                            //                                namesArray.append(street!)
+                            //                                namesArray.append(lat!)
+                            //
+                            //
+                            //                                print(names)
+                            //                                print("olawepo in ")
+                            //                            }
                         }
                         
-                        print(namesArray)
+                        // print(namesArray)
                     }
                     catch {
                         
